87f6ab610c86d0951641ba632466e1bff0b5e2a1
ilana-haddad <ilana.haddad@mail.mcgill.ca>
2019-03-06 21:07:36 -0500

    first commit

---
+++ ecse428-assignmentb/src/main/java/com/ilana/arielle/ecse428_assignmentb/App.java
@@ -0,0 +1,13 @@
+package com.ilana.arielle.ecse428_assignmentb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
